af7b72e7450b92f8d10f249b8d50c5f53a2faa22
Brijesh1990 <bkpandey.pandey@gmail.com>
2026-07-25 15:43:39 +0530

    25-07-2026 all data uploaded

--- reactjs/module-1-javascript&advanced javascript-ES6.0/essentials-javascript/calc-app/js/main.js
+++ reactjs/module-1-javascript&advanced javascript-ES6.0/essentials-javascript/calc-app/js/main.js
@@ -8,3 +8,37 @@ function inpValue(val)
 {
     document.getElementById("result").value+=val;
 }
+// create a function for slice last or end  values
+function slcApp()
+{
+    var sl=document.getElementById("result").value;
+    var res=sl.slice(0,-1);
+    document.getElementById("result").value=+res;
+}
+
+// create a function for airthematic  calculation
+
+function finalResult()
+{
+
+ var x=document.getElementById("result").value;
+ var y=eval(x);
+ document.getElementById("result").value=y;   
+    
+}
+
+// create a function for find square root 
+
+function sqRoot()
+{
+    var x=document.getElementById("result").value; 
+    var res=Math.sqrt(x);
+    document.getElementById("result").value=res;
+}
+// create a function for find pow  
+function sq1()
+{
+    var x=document.getElementById("result").value; 
+    var res=Math.pow(x,2);
+    document.getElementById("result").value=res;
+}
